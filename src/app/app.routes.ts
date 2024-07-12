a223fc5bafbbbb84663a9e01beed3b73d535eeb2
Christian Seip <christian.seip@ravitar.com>
2024-07-12 21:29:36 +0200

    404 page added.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
 import {PrivacyComponent} from "./pages/privacy/privacy.component";
 import {LegalNoticeComponent} from "./pages/legal-notice/legal-notice.component";
+import {NotFoundComponent} from "./pages/not-found/not-found.component";
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'privacy', component: PrivacyComponent},
   {path: 'legal-notice', component: LegalNoticeComponent},
+  {path: '**', component: NotFoundComponent}
 ];
